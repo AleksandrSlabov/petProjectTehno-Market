ffe3582745fb7bcb18f28809c97ce5314511b26a
SaSha <slabov2014@gmail.com>
2026-03-10 16:23:25 +0300

    fix footer and header

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import Main from "./components/Main/Main";
 import ModalCart from "./components/Modal/ModalCart/ModalCart";
 import ModalPlaceOrder from "./components/Modal/ModalPlaceOrder/ModalPlaceOrder";
 import Footer from "./components/footer/Footer";
-import "./App.scss";
 
 function App(props) {
   return (
     <>
       <ModalPlaceOrder />
       <ModalCart />
-      <Head title="Магазин TechnoMarket" />
+      <Head title="TechnoMarket" />
       <Main />
       <Footer />
     </>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.scss";
 import "./style/varibalse.scss";
 import "./style/utilities.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
 import { CartProvaider } from "./CartProvaider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,8 +14,3 @@ root.render(
     </CartProvaider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
